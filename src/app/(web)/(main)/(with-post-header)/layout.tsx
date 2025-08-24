9e7a68412db86dbf2eae341aa998c32bc73a5db7
shy-lemon <shy@lemoncloud.io>
2025-08-24 17:41:27 +0900

    style: fix PostLayout's media-query

--- src/app/(web)/(main)/(with-post-header)/layout.tsx
+++ src/app/(web)/(main)/(with-post-header)/layout.tsx
@@ -5,7 +5,7 @@ export default function PostLayout({
   children,
 }: Readonly<{ children: React.ReactNode }>) {
   return (
-    <div className="grid grid-cols-[3fr_7fr] divide-x divide-brand-100 border-x border-primary-100">
+    <div className="grid grid-cols-[2fr_8fr] max-lg:grid-cols-[3fr_7fr] divide-x divide-brand-100 border-x border-primary-100">
       <PostMenu />
       <div className="flex flex-col">
         <PostHeader />
